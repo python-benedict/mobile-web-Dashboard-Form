fa47b323d4b1874eb6ee2fdfca7b2cb28e573c39
Benedict <newlyf38@example.com>
2023-02-21 15:27:51 +0000

    created the views

--- mwsmsApp/views.py
+++ mwsmsApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import MyModel
+from .forms import MyForm
+
+def my_form(request):
+  if request.method == "POST":
+    form = MyForm(request.POST)
+    if form.is_valid():
+      form.save()
+  else:
+      form = MyForm()
+  return render(request, 'myapp/index.html', {'form': form})
